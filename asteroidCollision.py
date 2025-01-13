7d74c63676163943db7935e68fcc363f27ced83c
Ayad Masud <ayad.r.masud@gmail.com>
2025-01-13 12:47:02 -0600

    asteroid collision done

--- asteroidCollision.py
+++ asteroidCollision.py
@@ -4,4 +4,61 @@ class Solution(object):
         :type asteroids: List[int]
         :rtype: List[int]
         """
+        stack = []
+        for asteroid in asteroids: # loop through list
+            if asteroid > 0: # when number is pos
+                stack.append(asteroid)
+            else: # when the number is neg
+                if len(stack) == 0 or stack[-1] < 0:
+                    stack.append(asteroid)
+                else:
+                    while stack and asteroid < 0 < stack[-1]: # while there is still smt in stack
+                        if stack[-1] > abs(asteroid): # if the top of stack is larger
+                            break
+                        elif stack[-1] < abs(asteroid): # if the top of stack is smaller
+                            stack.pop()
+                        else: # when they are equal
+                            stack.pop()
+                            break
+                    else: # this else block is for the while loop, it only runs when the while loop terminates normally, as in it did not hit any break statements
+                        stack.append(asteroid)
+
+        return stack
+
+
+# from stack problem set
+# if two asteroids have the same sign they will never meet
+# Start from start of array, compare sign if same sign go to next num,
+# once sign is different, check which asteroid explodes
+
+# use a stack
+# if in positive direction push to stack
+# if in negative direction check stack, if empty or also negative, push to stack
+# if not compare values and pop whichever is smaller
+
+# empty list is false
+# non empty list is true
+
+asteroids = [-2, -2, 1, -2]
+stack = []
+
+for asteroid in asteroids:
+    if asteroid > 0: # when number is pos
+        stack.append(asteroid)
+    else: # when the number is neg
+        if len(stack) == 0 or stack[-1] < 0:
+            stack.append(asteroid)
+        else:
+            while stack and asteroid < 0 < stack[-1]:
+                if stack[-1] > abs(asteroid):
+                    break
+                elif stack[-1] < abs(asteroid):
+                    stack.pop()
+                else: # when they are equal
+                    stack.pop()
+                    break
+            else: # this else block is for the while loop, it only runs when the while loop terminates normally, as in it did not hit any break statements
+                stack.append(asteroid)
         
+print(stack) 
+
--- reverseLinkedList.py
+++ reverseLinkedList.py
@@ -9,4 +9,9 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
+
+#self.head (head) refers the the pointer pointing to the head of linked list
+# use curr, prev, and head pointers
+# prev should point at new head of reversed list (currently last node of of list)
+
         
