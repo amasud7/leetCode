2f193b1593442e21f2b74a305d8ca9eb48ce2524
Ayad Masud <ayad.r.masud@gmail.com>
2025-01-19 18:22:48 -0600

    max depth binary tree done

--- maxDepthBinaryTree.py
+++ maxDepthBinaryTree.py
@@ -10,4 +10,11 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: int
         """
+        # base case
+        if not root:
+            return 0
         
+        # recursive case (we do post order traversal bc we want to know the results of both left and right before making a decision)
+        leftDepth = self.maxDepth(root.left)
+        rightDepth = self.maxDepth(root.right)
+        return 1 + max(leftDepth, rightDepth) # we add plus one because the root itself counts as a level in the depth of a tree.
